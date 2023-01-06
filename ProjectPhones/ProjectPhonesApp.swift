776622f6308057c2b910f7f76133c6fb1d9bc71d
samanteras <samanter@mail.ru>
2023-01-06 16:47:25 +0300

    Create project

---
+++ ProjectPhones/ProjectPhonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectPhonesApp.swift
+//  ProjectPhones
+//
+//  Created by MAC13 on 06.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProjectPhonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
